cfbc6e138f0cadcf329691fc8d173dfd92556ec2
integralxdx <starbruce1@gmail.com>
2022-07-19 08:58:24 +0100

    try

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+able File  13 lines (11 sloc)  285 Bytes
+
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    ret = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
